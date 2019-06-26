90c290da9d118434be63a45994be0ce52183fbf7
dm1031 <danmdrums@gmail.com>
2019-06-26 11:20:37 -0400

    spec file

--- tests/models.spec.js
+++ tests/models.spec.js
@@ -3,21 +3,27 @@ const syncAndSeed = require('./test-seed')
 const db = require('../server/db/db')
 
 describe('our models', () => {
-  beforeAll(async () => {
-    await syncAndSeed()
+  beforeAll(() => {
+    return syncAndSeed()
   })
   afterAll(() => {
     return db.close().then(() => console.log('test db connection closed!'))
   })
+  test('Users are created', () => {
+    return User.findAll().then(users => expect(users.length).toBe(5))
+  })
   test('User must have an email', () => {
-    return User.findOne({
+    return User.findAll({
       where: {
         email: null
       }
-    })
-      .then(() => {
-        throw new Error('User has null email')
-      })
-      .catch(err => expect(err).not.toBe('User has null email'))
+    }).then(user => expect(user.length).toBe(0))
+  })
+  test('product must have a categoryId', () => {
+    return Product.findAll({
+      where: {
+        categoryId: null
+      }
+    }).then(product => expect(product.length).toBe(0))
   })
 })
